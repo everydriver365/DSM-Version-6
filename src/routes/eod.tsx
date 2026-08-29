1ec8d56713cf64298996ed80bc7c52ef1ac33940
Ken <kenneth@dufosse.co.uk>
2026-08-29 14:36:47 +0100

    Remove duplicate dead routes

--- src/routes/eod.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/eod")({
-  beforeLoad: () => {
-    throw redirect({ to: "/end-of-day" });
-  },
-});
